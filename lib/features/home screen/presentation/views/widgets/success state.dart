c28b1ffcc2e47ba384b5e07bdc37465334a55580
Mahmoud swilam <123952365+swilam202@users.noreply.github.com>
2023-08-19 15:39:24 +0300

    add permissions

--- lib/features/home screen/presentation/views/widgets/success state.dart
+++ lib/features/home screen/presentation/views/widgets/success state.dart
@@ -4,45 +4,47 @@ import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
-
-
 class SuccessState extends StatefulWidget {
-  const SuccessState({super.key,required this.path});
+  const SuccessState({super.key, required this.path});
 
   final String path;
+
   @override
   State<SuccessState> createState() => _SuccessStateState();
 }
 
 class _SuccessStateState extends State<SuccessState> {
-
   late VideoPlayerController videoPlayerController;
   late ChewieController chewieController;
-
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
 
-    videoPlayerController =  VideoPlayerController.file(File(widget.path))..initialize().then((_){
-      chewieController =  ChewieController(videoPlayerController: videoPlayerController,autoPlay: true,aspectRatio: videoPlayerController.value.aspectRatio,);
-      setState(() {
-
+    videoPlayerController = VideoPlayerController.file(File(widget.path))
+      ..initialize().then((_) {
+        chewieController = ChewieController(
+            videoPlayerController: videoPlayerController,
+            autoPlay: true,
+            aspectRatio: videoPlayerController.value.aspectRatio,
+            fullScreenByDefault: true,);
+        setState(() {});
       });
-    });
   }
-
-
-
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: videoPlayerController.value.isInitialized ? AspectRatio(
-        aspectRatio: 2.5/1.5, //videoPlayerController.value.aspectRatio,
-        child: Chewie(controller: chewieController),
-      ):SizedBox(),
+      child: videoPlayerController.value.isInitialized
+          ? AspectRatio(
+              aspectRatio: videoPlayerController.value.aspectRatio,
+              //videoPlayerController.value.aspectRatio,
+              child: Chewie(controller: chewieController),
+            )
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
     );
   }
 }
